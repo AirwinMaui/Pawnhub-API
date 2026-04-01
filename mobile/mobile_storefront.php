031742834c3f7dfacd2dfb864979d61f05e4bafc
AirwinMaui <delrosarioairwin708@gmail.com>
2026-04-02 01:57:49 +0800

    Debug storefront step

--- mobile/mobile_storefront.php
+++ mobile/mobile_storefront.php
@@ -1,6 +1,14 @@
 <?php
+declare(strict_types=1);
+
 header("Content-Type: application/json; charset=UTF-8");
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+require_once __DIR__ . "/../db.php";
+
 echo json_encode([
     "success" => true,
-    "message" => "storefront marker 12345"
+    "message" => "after require",
+    "has_pdo" => isset($pdo)
 ]);
