05d1d57867301eedf30b77e13a8501b371eecbaf
mendozakiaro-jpg <mendozakiaro@gmail.com>
2026-04-03 12:58:37 +0800

    Update customer_register.php

--- customer_register.php
+++ customer_register.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 ini_set("display_errors", "0");
 error_reporting(E_ALL);
 
-require_once __DIR__ . "/../db.php";
+require_once __DIR__ . "/db.php";
 
 function respond(int $statusCode, array $payload): void
 {
