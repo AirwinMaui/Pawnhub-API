298cd65e0dade551fcb43e9c2c922709d8dfed43
AirwinMaui <delrosarioairwin708@gmail.com>
2026-04-26 15:44:57 +0800

    Route mobile pawn transactions through index endpoint

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo "API is live";
+
+$action = $_GET['action'] ?? '';
+
+if ($action === 'mobile_pawn_transactions') {
+    require __DIR__ . '/mobile/mobile_pawn_transactions.php';
+    exit;
+}
+
+echo 'API is live';
