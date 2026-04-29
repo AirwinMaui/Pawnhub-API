62895b14dd69394885eeddb5499e2017c443e1e8
AirwinMaui <delrosarioairwin708@gmail.com>
2026-04-29 15:35:21 +0800

    Fix mobile pawn request upload issue (image size / multipart handling)

--- mobile/mobile_create_pawn_transaction.php
+++ mobile/mobile_create_pawn_transaction.php
@@ -6,6 +6,12 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Content-Type: application/json; charset=UTF-8');
 
+ini_set('upload_max_filesize', '25M');
+ini_set('post_max_size', '30M');
+ini_set('max_file_uploads', '10');
+ini_set('max_execution_time', '120');
+ini_set('memory_limit', '256M');
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit;
